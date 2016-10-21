d514801eb792bea56aa5302cd62e32bf06983357
podstreshniyv <podstreshniyv@SYDNTB138>
2016-10-21 18:37:22 +1100

    http

--- Angular2Solution/Angular2/app/hero-detail.component.ts
+++ Angular2Solution/Angular2/app/hero-detail.component.ts
@@ -30,4 +30,8 @@ export class HeroDetailComponent implements OnInit{
         window.history.back();
     }
 
+    save(): void {
+        this.heroService.update(this.hero).then(() => this.goBack());
+    } 
+
 }
--- Angular2Solution/Angular2/app/heroes.component.ts
+++ Angular2Solution/Angular2/app/heroes.component.ts
@@ -5,20 +5,20 @@ import {Hero} from "./hero";
 import { HeroService } from './hero.service';
 
 @Component(({
-    selector: "my-heroes" ,
+    selector: "my-heroes",
     templateUrl: "app/heroes.component.html",
     styleUrls: ['app/heroes.component.css']
 
 }) as any)
 export class HeroesComponent implements OnInit {
-    constructor(private heroService: HeroService, private router: Router) {}
+    constructor(private heroService: HeroService, private router: Router) { }
 
     ngOnInit(): void {
         this.heroService.getHeroes().then(x => this.heroes = x);
     }
-    
+
     selectedHero: Hero;
-    heroes: Hero[]; 
+    heroes: Hero[];
     //get heroes(): Hero[] {
     //    return this._heroes;
     //   // return this.heroService.getHeroes();
@@ -30,7 +30,31 @@ export class HeroesComponent implements OnInit {
         this.selectedHero = hero;
     }
     gotoDetail(): void {
-      //  let link = ['/detail', hero.id];
+        //  let link = ['/detail', hero.id];
         this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) {
+            return;
+        }
+        this.heroService.create(name)
+            .then(() => {
+                this.heroService.getHeroes()
+                    .then(x => {
+                        this.heroes = x;
+                        this.selectedHero = x.find(y => y.name === name);
+                    });
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroService
+            .delete(hero.id)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            });
     }
 }
